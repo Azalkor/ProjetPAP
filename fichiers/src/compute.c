519cf570afb5a1d13d3d7c7b0e62098028ce5d7a
Dylan Bedin <dylan.bedin@gmail.com>
2017-04-30 18:00:49 +0200

    corrections swap + début openmp task

--- fichiers/src/compute.c
+++ fichiers/src/compute.c
@@ -18,11 +18,13 @@ unsigned compute_v3(unsigned nb_iter);
 unsigned compute_v0_openmpfor(unsigned nb_iter);
 unsigned compute_v1_openmpfor(unsigned nb_iter);
 unsigned compute_v2_openmpfor(unsigned nb_iter);
+unsigned compute_v1_openmptask(unsigned nb_iter);
 
 void_func_t first_touch [] = {
   NULL,
   NULL,//first_touch_v1,
   NULL,//first_touch_v2,
+  NULL,
   NULL,
   NULL,
   NULL,
@@ -35,6 +37,7 @@ int_func_t compute [] = {
   compute_v0_openmpfor,
   compute_v1_openmpfor,
   compute_v2_openmpfor,
+  compute_v1_openmptask,
 };  //  compute_v3,
 
 char *version_name [] = {
@@ -44,12 +47,14 @@ char *version_name [] = {
   "Séquentielle de base avec OpenMP for",
   "Séquentielle tuilée avec OpenMP for",
   "Séquentielle tuilée (optimisée) avec OpenMP for",
+  "Séquentielle tuilée avec OpenMP task",
 };//  "OpenMP",
 //  "OpenMP zone",
 //  "OpenCL",
 
 
 unsigned opencl_used [] = {
+  0,
   0,
   0,
   0,
@@ -66,8 +71,9 @@ unsigned const colorMort = 0x0;
 
 int countAlive(int x, int y);
 void majImg(int i, int j, int nbVoisinsVivants);
-void copyTab();
+void majTab();
 void initTabsTiles();
+void task_v1(int i);
 
 unsigned compute_v0 (unsigned nb_iter){
   for (unsigned it = 1; it <= nb_iter; it++) {
@@ -76,8 +82,8 @@ unsigned compute_v0 (unsigned nb_iter){
 	majImg(i, j, countAlive(i, j));
       }
     }
+    swap_images();
   }
-  swap_images();
   return 0;
 }
 
@@ -110,8 +116,8 @@ unsigned compute_v1(unsigned nb_iter){
 	}
       }
     }
+    swap_images();
   }
-  swap_images();
   return 0;
 }
 
@@ -127,10 +133,34 @@ unsigned compute_v1_openmpfor(unsigned nb_iter){
 	}
       }
     }
+    swap_images();
   }
-  swap_images();
   return 0;
 }
+
+unsigned compute_v1_openmptask(unsigned nb_iter){
+  for (unsigned it = 1; it <= nb_iter; it++) {
+#pragma omp parallel
+#pragma omp single
+    for(int i = 0; i < DIM; i+=sizeTiles){
+#pragma omp task
+      task_v1(i);
+    }
+    swap_images();
+  }
+  return 0;
+}
+
+void task_v1(int i){
+  for(int j = 0; j < DIM; j+=sizeTiles){
+    for(int x = i; x < i + sizeTiles; x++){
+      for(int y = i; y < j + sizeTiles; y++){
+	majImg(i, j, countAlive(i, j));
+      }
+    }
+  }
+}
+
 
 ///////////////////////////// Version OpenMP optimisée
 
@@ -159,9 +189,9 @@ unsigned compute_v2(unsigned nb_iter){
 	}
       }
     }
+    majTab();
+    swap_images();
   }
-  copyTab();
-  swap_images();
   return 0; // on ne s'arrête jamais
 }
 
@@ -182,9 +212,9 @@ unsigned compute_v2_openmpfor(unsigned nb_iter){
 	}
       }
     }
+    majTab();
+    swap_images();
   }
-  copyTab();
-  swap_images();
   return 0; // on ne s'arrête jamais
 }
 
@@ -199,14 +229,14 @@ unsigned compute_v3 (unsigned nb_iter)
 
 unsigned compute_v0_openmpfor(unsigned nb_iter){
   for (unsigned it = 1; it <= nb_iter; it++) {
-    //#pragma omp for
+#pragma omp for
     for(unsigned i = 0; i < DIM; i++){
       for(unsigned j = 0; j < DIM; j++){
 	majImg(i, j, countAlive(i, j));
       }
     }
+    swap_images();
   }
-  swap_images();
   return 0;
 }
 
@@ -272,7 +302,7 @@ int min(int a, int b){
   return b;
 }
 
-void copyTab()
+void majTab()
 {
   for(int x =0;x<NBTILES;x++){
     for (int y = 0; y<NBTILES;y++){
